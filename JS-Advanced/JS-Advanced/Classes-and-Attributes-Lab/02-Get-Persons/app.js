09039e7ddfcfc03a3bc240311dc8164069f5372a
BorislavChernev <borocool359@gmail.com>
2022-10-26 22:00:22 +0300

    Completed Task

---
+++ JS-Advanced/JS-Advanced/Classes-and-Attributes-Lab/02-Get-Persons/app.js
@@ -0,0 +1,25 @@
+function solve() {
+	class Person {
+		constructor(firstName, lastName, age, email) {
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.age = age;
+			this.email = email;
+		}
+
+		toString() {
+			return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`;
+		}
+	}
+
+	let anna = new Person('Anna', 'Simpson', 22, 'anna@yahoo.com');
+	let softuni = new Person('SoftUni')
+	let stephan = new Person('Stephan', 'Johnson', 25);
+	let gabriel = new Person('Gabriel', 'Peterson', 24, 'g.p@gmail.com');
+
+	return [anna, softuni, stephan, gabriel];
+}
+
+let people = solve();
+
+console.table(people);
