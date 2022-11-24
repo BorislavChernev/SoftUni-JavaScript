5a00f642aa9fdc271572406d7445ed22e7f709bf
BorislavChernev <borocool359@gmail.com>
2022-11-24 18:13:20 +0200

    Completed Task

---
+++ JS-Advanced/JS-Advanced/DOM-Introduction-Lab/04.Show More/ShowMore.js
@@ -0,0 +1,7 @@
+function showText() {
+    let toHide = document.getElementById('more');
+    let text = document.getElementById('text');
+
+    toHide.style.display = 'none';
+    text.style.display = 'inline';
+}
