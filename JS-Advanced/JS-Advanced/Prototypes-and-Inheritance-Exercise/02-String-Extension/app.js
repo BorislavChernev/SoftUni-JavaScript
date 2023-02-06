14e06514cda646e59314718e883363fd53d8fa00
BorislavChernev <borocool359@gmail.com>
2023-02-06 18:47:57 +0200

    Completed Task

---
+++ JS-Advanced/JS-Advanced/Prototypes-and-Inheritance-Exercise/02-String-Extension/app.js
@@ -0,0 +1,22 @@
+(function solve() {
+    String.prototype.ensureStart(str) = function () {
+        return this.startsWith(str)
+    }
+
+     String.prototype.ensureEnd(str) = function () {
+        return this.endsWith(str);
+     }
+        
+     String.prototype.isEmpty() = function () {
+        return this.length === 0;
+     }
+        
+     String.prototype.truncate(n) = function () {
+
+     }
+        
+     String.prototype.format(string, ...params) = function () {
+
+     }
+    
+})()
