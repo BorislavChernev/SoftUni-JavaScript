9dc325687c3afa3b2c5446d001a1b622680c4dd2
BorislavChernev <borocool359@gmail.com>
2023-02-06 18:47:53 +0200

    Completed Task

---
+++ JS-Advanced/JS-Advanced/Prototypes-and-Inheritance-Exercise/01-Array-Extension/app.js
@@ -0,0 +1,21 @@
+(function solve() {
+	Array.prototype.last = function () {
+		return this[this.length - 1];
+	};
+
+	Array.prototype.skip = function (n) {
+		return this.slice(n);
+	};
+
+	Array.prototype.take = function (n) {
+		return this.slice(0, n);
+	};
+
+	Array.prototype.sum = function () {
+		return this.reduce((first, second) => first + second);
+	};
+
+	Array.prototype.average = function () {
+		return this.sum() / this.length;
+	};
+})()
