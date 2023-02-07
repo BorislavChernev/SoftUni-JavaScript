33d6f86d6f9f98ba2bdaf985c77da1134fd87ce6
BorislavChernev <borocool359@gmail.com>
2023-02-07 17:05:29 +0200

    Completed Task

---
+++ JS-Advanced/JS-Advanced/Syntax-Functions-and-Statements-Exercise/08-Validity-Checker.js
@@ -0,0 +1,27 @@
+function validityChecker(x1, y1, x2, y2) {
+	function distance(x1, y1, x2, y2) {
+		let distanceX = x1 - x2;
+		let distanceY = y1 - y2;
+		return Math.sqrt(distanceX ** 2 + distanceY ** 2);
+	}
+
+	if (Number.isInteger(distance(x1, y1, 0, 0))) {
+		console.log(`{${x1}, ${y1}} to {0, 0} is valid`);
+	} else {
+		console.log(`{${x1}, ${y1}} to {0, 0} is invalid`);
+	}
+
+	if (Number.isInteger(distance(x2, y2, 0, 0))) {
+		console.log(`{${x2}, ${y2}} to {0, 0} is valid`);
+	} else {
+		console.log(`{${x2}, ${y2}} to {0, 0} is invalid`);
+	}
+
+	if (Number.isInteger(distance(x1, y1, x2, y2))) {
+		console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is valid`);
+	} else {
+		console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is invalid`);
+	}
+}
+
+validityChecker(2, 1, 1, 1);
